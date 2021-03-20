d752979675827672e052ec81b685fe4dfca20d42
arahal81 <arahal81@gmail.com>
2021-03-20 22:38:16 +0200

    backup

--- js/app.js
+++ js/app.js
@@ -1,24 +1,48 @@
 `use strict`;
-let students = [];
-let table = document.getElementById('tableData');
-function Student(name, email, age, tuition) {
-    this.id = students.length + 1;
+Student.sL = [];
+let tableDiv = document.getElementById('tableDiv');
+function Student(name, email, phone, age, tuition, id) {
+    this.id = id;
     this.name = name;
     this.email = email;
+    this.phone = phone;
     this.age = age;
     this.tuition = tuition;
-    students.push(this);
+    Student.sL.push(this);
 }
 
-/*const StudentsList=function(St){
-    
-
-}*/
 
 
+
+function total(params) {
+    let tot = 0;
+    for (let i in Student.sL) {
+
+        tot += parseInt(Student.sL[i].tuition);
+    }
+    return tot;
+}
+/*
+const StudentsList = function (stL) {
+    this.sL = stL;
+
+}
+StudentsList.prototype.add = function (st) {
+    this.sL.push(st);
+}
+
+*/
 
 Student.prototype.SetLocalStorage = function () {
-    localStorage.setItem('students', JSON.stringify(students[students.length - 1]));
+    localStorage.setItem('students', JSON.stringify(Student.sL));
+}
+Student.prototype.getLocalStorage = function () {
+    const temp = JSON.parse(localStorage.getItem('students'));
+    if (temp !== null) {
+        Student.sL = temp;
+    }
+
+
 }
 
 
@@ -30,70 +54,87 @@ let form = document.getElementById('form');
 form.addEventListener('submit', submitStudentForm);
 function submitStudentForm(event) {
     event.preventDefault();
-    console.log(event);
     let age = randumAge();
-    console.log(age);
     let email = event.target.email.value;
     let name = email.split("@")[0];
     let tuition = event.target.fee.value;
-    new Student(name, email, age, tuition);
-    console.log(students);
+    let phone = event.target.phone.value;
+    let id;
+    if (Student.sL.length === 0) {
+        id = 1;
+    } else { id = Student.sL.length + 1 }
+
+    new Student(name, email, phone, age, tuition, id);
+    // console.log(students);
     Student.prototype.SetLocalStorage();
+    //StudentsList.prototype.add(sTemp);
+
     renderTable()
-
+    form.reset();
 }
-
+renderTable()
 
 function renderTable() {
 
-    document.getElementById('tableData').innerHTML = "";
+    Student.prototype.getLocalStorage();
+    tableDiv.innerHTML = "";
+    let table = document.createElement('table');
+    tableDiv.appendChild(table);
     let trH = document.createElement('tr');
     table.appendChild(trH);
-    let thId = Document.createElement('th')
+    let thId = document.createElement('th');
     trH.appendChild(thId);
     thId.textContent = "Id";
-    let thName = Document.createElement('th')
+    let thName = document.createElement('th');
     trH.appendChild(thName);
     thName.textContent = "Name";
-    let thEmail = Document.createElement('th')
+    let thEmail = document.createElement('th');
     trH.appendChild(thEmail);
     thEmail.textContent = "Email";
 
 
-    let thMobile = Document.createElement('th')
+    let thMobile = document.createElement('th');
     trH.appendChild(thMobile);
     thMobile.textContent = "Mobile";
-    let thAge = Document.createElement('th')
+    let thAge = document.createElement('th');
     trH.appendChild(thAge);
     thAge.textContent = "Age";
-    let thTuition = Document.createElement('th')
+    let thTuition = document.createElement('th');
     trH.appendChild(thTuition);
     thTuition.textContent = "Tuition";
 
-    for (let i in students) {
+    for (let i in Student.sL) {
 
         let tr = document.createElement('tr');
         table.appendChild(tr);
         let tdID = document.createElement('td');
         tr.appendChild(tdID)
-        tdID.textContent = students[i].id
+        //console.log(students);
+        tdID.textContent = Student.sL[i].id;
 
         let tdName = document.createElement('td');
         tr.appendChild(tdName)
-        tdName.textContent = students[i].name
+        tdName.textContent = Student.sL[i].name;
 
 
         let tdEmail = document.createElement('td');
         tr.appendChild(tdEmail)
-        tdEmail.textContent = students[i].email
+        tdEmail.textContent = Student.sL[i].email;
 
         let tdMoblie = document.createElement('td');
         tr.appendChild(tdMoblie)
-        tdMoblie.textContent = students[i].name
+        tdMoblie.textContent = Student.sL[i].phone;
+        let tdAge = document.createElement('td');
+        tr.appendChild(tdAge)
+        tdAge.textContent = Student.sL[i].age
         let tdTuition = document.createElement('td');
         tr.appendChild(tdTuition)
-        tdTuition.textContent = students[i].tuition
+        tdTuition.textContent = Student.sL[i].tuition
     }
+    let tot = total();
+    let h4 = document.createElement('h4');
+    tableDiv.appendChild(h4);
+    h4.textContent = `Total: ${tot}`;
 }
 
 
